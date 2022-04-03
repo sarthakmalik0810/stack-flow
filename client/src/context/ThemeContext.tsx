7f6e7dd2cbb9be5f615823180e407560580bb85f
Sarthak Malik <sarthak.malik@simpplr.com>
2022-04-03 14:25:30 +0530

    added prettierrc

--- client/src/context/ThemeContext.tsx
+++ client/src/context/ThemeContext.tsx
@@ -1,60 +1,60 @@
-import React, { ReactChildren, useEffect, useState } from "react";
+import React, { ReactChildren, useEffect, useState } from 'react'
 
 interface ThemeContextInterface {
-  theme: string;
-  setTheme: (theme: string) => void;
+  theme: string
+  setTheme: (theme: string) => void
 }
 
 export const ThemeContext = React.createContext<ThemeContextInterface | null>(
   null
-);
+)
 
 const getInitialTheme = (): string => {
   if (typeof window !== undefined && window.localStorage) {
-    const storedPref = window.localStorage.getItem("color-theme");
-    if (typeof storedPref === "string") {
-      return storedPref;
+    const storedPref = window.localStorage.getItem('color-theme')
+    if (typeof storedPref === 'string') {
+      return storedPref
     }
-    const userMedia = window.matchMedia("prefers-color-scheme: dark");
+    const userMedia = window.matchMedia('prefers-color-scheme: dark')
     if (userMedia.matches) {
-      return "dark";
+      return 'dark'
     }
   }
   /** default theme is light */
-  return "light";
-};
+  return 'light'
+}
 
 export interface ThemeProviderProps {
-  initialTheme: string;
-  children: ReactChildren;
+  initialTheme: string
+  children: ReactChildren
 }
 
 export const ThemeProvider = ({
   initialTheme,
   children,
 }: ThemeProviderProps) => {
-  const [theme, setTheme] = useState(getInitialTheme);
+  const [theme, setTheme] = useState(getInitialTheme)
 
   const rawSetTheme = (theme: string): void => {
-    const root = window.document.documentElement;
-    const isDark = theme === "dark";
-    root.classList.remove(isDark ? "light" : "dark");
-    root.classList.add(theme);
+    const root = window.document.documentElement
+    const isDark = theme === 'dark'
+    root.classList.remove(isDark ? 'light' : 'dark')
+    root.classList.add(theme)
 
-    window.localStorage.setItem("color-theme", theme);
-  };
+    window.localStorage.setItem('color-theme', theme)
+  }
 
   if (initialTheme) {
-    rawSetTheme(initialTheme);
+    rawSetTheme(initialTheme)
   }
 
   useEffect(() => {
-    rawSetTheme(theme);
-  }, [theme]);
+    rawSetTheme(theme)
+  }, [theme])
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
       {children}
     </ThemeContext.Provider>
-  );
-};
+  )
+}
